a3ce3a81a733d3d62a008b89e1489e03905c2396
Shiva Chavoshian <chavoshian.shiva@gmail.com>
2018-09-08 16:09:54 +0430

    final changes and fix some bug

--- convert_to_jalali.py
+++ convert_to_jalali.py
@@ -48,3 +48,17 @@ def convert_dates(conn):
    
     return(jalali_list)
 
+conn_module = create_connection('bandwidth.db')
+data = convert_dates(conn_module)
+conn = sqlite3.connect('bandwidth_jalali.db')
+
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE jalali
+             (date text, RX real, TX real)''')
+
+for item in data:
+    cur.execute("INSERT INTO jalali VALUES ('{0}',{1},{2})".format(item[0], item[1], item[2]))
+    conn.commit()
+
+conn.close()
