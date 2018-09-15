2903cd39483eb3c06e52714a6afa76a1766b4baf
Shiva Chavoshian <chavoshian.shiva@gmail.com>
2018-09-15 10:49:23 +0430

    last commit

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-# This script delete old database and update daily database from server with rsync
+### This script delete old database and update daily database from server with rsync
 
 echo  "##################"
 echo  "bandwidth info sync started at:"
 date  +%c
 echo  "##################"
 
+DB_BACKUP_FILE="${TODAY}/bandwidth.tar.bz2"
 
 cd "/home/shiva/w/bandwidth"
 CWD="$(pwd)"
 echo $CWD
 
+# Archive old db 
+tar -czvf "${CWD}${DB_BACKUP_FILE}" "bandwidth.db"
+
 # delete old jalali database
 rm bandwidth_jalali.db
 
 # rsync new database
-rsync -azP --stats --progress --times --delete sls-streamer:/home/navaak/bandwidth.db /home/shiva/w/bandwidth
+rsync -azP --stats --progress --times sls-streamer:/home/navaak/bandwidth.db ${CWD}
 
-# run convert script
+# run convert script to create new jalali db 
 python3 convert_to_jalali.py
 
 
